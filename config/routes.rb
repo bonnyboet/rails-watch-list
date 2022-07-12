50439fa4f96350f8ac2bf19a37dcf29ebb45d5f9
Bonny Boet <bonny.boet@gmail.com>
2022-07-12 21:13:46 +0900

    Updated routes to include root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
+
   resources :lists, only: [ :index, :new, :create, :show ] do
     resources :bookmarks, only: [ :new, :create, :destroy ]
   end
